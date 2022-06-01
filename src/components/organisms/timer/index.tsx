6f5c2a97bbf1992f3b80f0a6e1fc25e749f97484
Humberto Mangino <hmangino@gmail.com>
2022-06-01 16:01:23 -0600

    Fix reset

--- src/components/organisms/timer/index.tsx
+++ src/components/organisms/timer/index.tsx
@@ -1,4 +1,10 @@
-import React, { ChangeEvent, ReactNode, useEffect, useState } from 'react';
+import React, {
+  ChangeEvent,
+  useEffect,
+  useState,
+  createRef,
+  useRef,
+} from 'react';
 import './styles.css';
 import {
   Avatar,
@@ -16,17 +22,57 @@ import AvTimerIcon from '@mui/icons-material/AvTimerOutlined';
 type InputEvent = {
   event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
 };
+
+const defaultTimeDigits = [''];
+const defaultTimeView = '00h 00m 00s';
+const defaultTimeUnits = [0, 0, 0];
 // ToDo: Implement coutdown functionality
 function Timer() {
-  const [timeDigits, setTimeDigits] = useState(['']);
-  const [timeUnits, setTimeUnits] = useState([0, 0, 0]);
+  const [timeDigits, setTimeDigits] = useState(defaultTimeDigits);
+  const [timeView, setTimeView] = useState(defaultTimeView);
+  const [timeUnits, setTimeUnits] = useState(defaultTimeUnits);
   const [ctaStartStop, setCtaStartStop] = useState(false);
   const [hasFocus, setHasFocus] = useState(false);
+
+  const inputRef: any = createRef();
 
   useEffect(() => {
     console.log(timeDigits);
     console.log('timeUnits ', timeUnits);
-  }, [timeDigits, timeUnits]);
+    console.dir(inputRef.current.value);
+  }, [timeDigits, timeUnits, inputRef]);
+
+  useEffect(() => {
+    let cleanup = inputRef.current;
+    if (timeDigits.length === 1) {
+      setTimeView(`00h 00m ${timeDigits[0] || '00'}s`);
+    } else if (timeDigits.length === 2) {
+      setTimeView(`00h 00m ${timeDigits[0]}${timeDigits[1]}s`);
+    } else if (timeDigits.length === 3) {
+      setTimeView(`00h 0${timeDigits[0]}m ${timeDigits[1]}${timeDigits[2]}s`);
+    } else if (timeDigits.length === 4) {
+      setTimeView(
+        `00h ${timeDigits[0]}${timeDigits[1]}m ${timeDigits[2]}${timeDigits[3]}s`,
+      );
+    } else if (timeDigits.length === 5) {
+      setTimeView(
+        `0${timeDigits[0]}h ${timeDigits[1]}${timeDigits[2]}m ${timeDigits[3]}${timeDigits[4]}s`,
+      );
+    } else if (timeDigits.length === 6) {
+      setTimeView(
+        `${timeDigits[0]}${timeDigits[1]}h ${timeDigits[2]}${timeDigits[3]}m ${timeDigits[4]}${timeDigits[5]}s`,
+      );
+    } else {
+      setTimeView(defaultTimeView);
+    }
+    return () => {
+      if (!hasFocus) cleanup.value = '';
+    };
+  }, [timeView, timeDigits, inputRef, hasFocus]);
+
+  // useEffect(() => {
+  //   setInterval(() => {}, 100);
+  // });
 
   function handleTimeInput(e: InputEvent['event']) {
     let timeChunk = e.target.value;
@@ -41,26 +87,10 @@ function Timer() {
     ctaStartStop ? setCtaStartStop(false) : setCtaStartStop(true);
   }
 
-  function renderTimeView(): ReactNode {
-    if (timeDigits.length === 1) {
-      return `00h 00m ${timeDigits[0] || '00'}s`;
-    } else if (timeDigits.length === 2) {
-      return `00h 00m ${timeDigits[0]}${timeDigits[1]}s`;
-    } else if (timeDigits.length === 3) {
-      return `00h 0${timeDigits[0]}m ${timeDigits[1]}${timeDigits[2]}s`;
-    } else if (timeDigits.length === 4) {
-      return `00h ${timeDigits[0]}${timeDigits[1]}m ${timeDigits[2]}${timeDigits[3]}s`;
-    } else if (timeDigits.length === 5) {
-      return `0${timeDigits[0]}h ${timeDigits[1]}${timeDigits[2]}m ${timeDigits[3]}${timeDigits[4]}s`;
-    } else if (timeDigits.length === 6) {
-      return `${timeDigits[0]}${timeDigits[1]}h ${timeDigits[2]}${timeDigits[3]}m ${timeDigits[4]}${timeDigits[5]}s`;
-    } else {
-      return `00h 00m 00s`;
-    }
-  }
-
   function renderTimer() {
-    return `${timeUnits[0]}h ${timeUnits[1]}m ${timeUnits[2]}s`;
+    return `${timeUnits[0] || '00'}h ${timeUnits[1] || '00'}m ${
+      timeUnits[2] || '00'
+    }s`;
   }
 
   function padTo2Digits(num: number) {
@@ -93,12 +123,19 @@ function Timer() {
     )}m ${padTo2Digits(timeUnits[2])}s `;
   }
 
+  function handleReset() {
+    setTimeDigits(defaultTimeDigits);
+    setTimeView(defaultTimeView);
+    setTimeUnits(defaultTimeUnits);
+  }
+
   function handleInputBlur() {
     handleTimeCalculation();
     setHasFocus(false);
   }
 
   function handleInputFocus() {
+    handleReset();
     setHasFocus(true);
   }
 
@@ -126,14 +163,14 @@ function Timer() {
               <Input
                 className="inputDataText"
                 onChange={(e) => handleTimeInput(e)}
-                inputProps={{ maxLength: 6 }}
+                inputProps={{ maxLength: 6, ref: inputRef }}
                 onBlur={handleInputBlur}
                 onFocus={handleInputFocus}
               />
             </Box>
             <Box className="inputViewContainer">
               <Typography gutterBottom variant="h5" component="div">
-                {hasFocus ? renderTimeView() : renderTimer()}
+                {hasFocus ? timeView : renderTimer()}
               </Typography>
             </Box>
           </Box>
@@ -142,7 +179,7 @@ function Timer() {
           <Button variant="contained" size="medium" onClick={handleStartStop}>
             {!ctaStartStop ? 'Start' : 'Stop'}
           </Button>
-          <Button variant="contained" size="medium">
+          <Button variant="contained" size="medium" onClick={handleReset}>
             Reset
           </Button>
         </CardActions>
